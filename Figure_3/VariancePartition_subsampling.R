7984dce7f591caa80175390d6160f18895cef640
Carradec <43535389+Carradec@users.noreply.github.com>
2022-10-04 16:25:04 +0200

    Update VariancePartition_subsampling.R

--- Figure_3/VariancePartition_subsampling.R
+++ Figure_3/VariancePartition_subsampling.R
@@ -9,8 +9,8 @@ if(length(args) < 1) {
 if("--help" %in% args) {
   cat("
 	--variables FILE samples in lignes (rownames) variable in columns (header)
-	--HostExpression FILE Table normalized for the host
-	--SymbiontExpression FILE Table normalized for the symbiont
+	--HostExpression FILE Table TPM normalized for the host
+	--SymbiontExpression FILE Table TPM normalized for the symbiont
 	--CPU INT number of available CPU
 	--OutSymbiont file name for the network graph
 	--OutHost file name for the network graph
@@ -45,12 +45,6 @@ writeLines("***\n\n")
 ##########################
 ###########CORE###########
 ##########################
-
-
-#article
-#https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-016-1323-z
-#r pipeline
-#https://bioconductor.org/packages/release/bioc/html/variancePartition.html
 #library(devtools)
 #install_github("GabrielHoffman/variancePartition")
 library(variancePartition)
@@ -63,91 +57,90 @@ rownames(Genet)<-Genet$Samples
 #Genet<-Genet[rowSums(cbind(is.na(Genet$PocilloGG),is.na(Genet$SymbioGG)))==0,]
 #Genet<-Genet[is.na(Genet$SymbioGG)==F,]
 
-
 #
-#Analysis of Cladocopium
+#Analysis of Cladocopium#
 #
-
 
 Total<-data.frame()
 #subsampling test
 tab <- read.table(argsL$SymbiontExpression, header=TRUE,stringsAsFactors = F)
 rownames(tab)<-tab$Gene
+
+#remove I04 and keep only samples with host and symbiont gene expression data.
 GenetC<-Genet[Genet$Islands!="I04",]
 GenetC<-GenetC[is.na(GenetC$SymbioGG)==F,]
 GenetC<-GenetC[GenetC$Samples%in%colnames(tab),]
-#Subsamples<-"All"
+
 Subsamples<-strsplit(unique(paste(as.vector(sample(GenetC$Samples)),as.vector(sample(GenetC$Samples))))[1:10]," ")
 for (i in 1:5){
-  print(paste("Symbiont Run ",i,"/100",sep=""))
-#Samples with genetic group information
-Subsample<-Subsamples[[i]]
-GenetC2<-GenetC[!(GenetC$Samples%in%Subsample),]
-datC<-tab[,colnames(tab)%in%GenetC2$Samples]
-#Expressed in 3 samples at least : 23474 genes and 82 samples
-datC<-datC[rowSums(datC>0)>=3,]
-#VariancePartition
-#removing batch effect first
+	print(paste("Symbiont Run ",i,"/100",sep=""))
+	#Samples with genetic group information
+	Subsample<-Subsamples[[i]]
+	GenetC2<-GenetC[!(GenetC$Samples%in%Subsample),]
+	datC<-tab[,colnames(tab)%in%GenetC2$Samples]
+	#Expressed in 3 samples at least : 23474 genes and 82 samples
+	datC<-datC[rowSums(datC>0)>=3,]
 
-residListC <- fitVarPartModel(datC, ~ (1|Batch), GenetC2,fxn=residuals,BPPARAM = param)
-residMatrixC <- do.call(rbind, residListC)
-# fit model on residuals
-form <- ~ (1|Islands) + (1|PocilloGG) + (1|SymbioGG)
-varPartC <- fitExtractVarPartModel(residMatrixC, form, GenetC2,BPPARAM = param)
-rm(residListC)
-rm(residMatrixC)
+	#VariancePartition
+	#removing batch effect first
+	residListC <- fitVarPartModel(datC, ~ (1|Batch), GenetC2,fxn=residuals,BPPARAM = param)
+	residMatrixC <- do.call(rbind, residListC)
+	# fit model on residuals
+	form <- ~ (1|Islands) + (1|PocilloGG) + (1|SymbioGG)
+	varPartC <- fitExtractVarPartModel(residMatrixC, form, GenetC2,BPPARAM = param)
+	rm(residListC)
+	rm(residMatrixC)
 
-#pdf(file="VarPart_batchcorrected_3Samples_woI04_Symbio.pdf")
-#plotVarPart(varPartC)
-#dev.off()
-
-
-df<-cbind(melt(as.matrix(varPartC)),SubSampling=paste(sort(Subsample),collapse="-"))
-colnames(df)<-c("Gene","Variable","Variance","SubSampling")
-df$Variance<-round(df$Variance,digits=4)
-Total<-rbind(Total,df)
+	#pdf(file="VarPart_batchcorrected_3Samples_woI04_Symbio.pdf")
+	#plotVarPart(varPartC)
+	#dev.off()
+	df<-cbind(melt(as.matrix(varPartC)),SubSampling=paste(sort(Subsample),collapse="-"))
+	colnames(df)<-c("Gene","Variable","Variance","SubSampling")
+	df$Variance<-round(df$Variance,digits=4)
+	Total<-rbind(Total,df)
 }
+
 write.table(file=argsL$OutSymbiont,row.names=F,Total,sep="\t",quote=F)
 
-####
-#Pocillo
-####
 #
 #Analysis of Pocillopora
 #
 
 tab<-read.table(file = argsL$HostExpression,h=T,sep="\t")
+
+#Keep only samples with host and symbiont gene expression data.
 GenetP<-Genet[is.na(Genet$PocilloGG)==F,]
 GenetP<-GenetP[GenetP$Samples%in%colnames(tab),]
 Total<-data.frame()
-#Subsamples<-"All"
 Subsamples<-strsplit(unique(paste(as.vector(sample(GenetP$Samples)),as.vector(sample(GenetP$Samples))))[1:10]," ")
+
 #subsampling test
 for (i in 1:5){
-  print(paste("Pocillopora Run ",i,"/100",sep=""))
-  Subsample<-Subsamples[[i]]
-  GenetP2<-GenetP[!(GenetP$Samples%in%Subsample),]
+	print(paste("Pocillopora Run ",i,"/100",sep=""))
+	Subsample<-Subsamples[[i]]
+	GenetP2<-GenetP[!(GenetP$Samples%in%Subsample),]
 	GenetP2
-  datP<-tab[,colnames(tab)%in%GenetP2$Samples]
+	datP<-tab[,colnames(tab)%in%GenetP2$Samples]
 
-#Expressed in 5 samples at least.
-datP<-datP[rowSums(datP>0)>=3,]
-#VariancePartition
-#removing batch effect first
+	#Expressed in 3 samples at least.
+	datP<-datP[rowSums(datP>0)>=3,]
+	#VariancePartition
+	#removing batch effect first
 
-residListP <- fitVarPartModel(datP, ~ (1|Batch), GenetP2,fxn=residuals,BPPARAM=param)
-residMatrixP <- do.call(rbind, residListP)
+	residListP <- fitVarPartModel(datP, ~ (1|Batch), GenetP2,fxn=residuals,BPPARAM=param)
+	residMatrixP <- do.call(rbind, residListP)
 
-# fit model on residuals
-form <- ~ (1|Islands) + (1|PocilloGG) + (1|SymbioGG)
-varPartP <- fitExtractVarPartModel(residMatrixP, form, GenetP2,BPPARAM=param)
-rm(residListP)
-rm(residMatrixP)
+	# fit model on residuals
+	form <- ~ (1|Islands) + (1|PocilloGG) + (1|SymbioGG)
+	varPartP <- fitExtractVarPartModel(residMatrixP, form, GenetP2,BPPARAM=param)
+	rm(residListP)
+	rm(residMatrixP)
 
-#random subsampling
-df<-cbind(melt(as.matrix(varPartP)),SubSampling=paste(sort(Subsample),collapse="-"))
-colnames(df)<-c("Gene","Variable","Variance","SubSampling")
-df$Variance<-round(df$Variance,digits=4)
-Total<-rbind(Total,df)
+	#random subsampling
+	df<-cbind(melt(as.matrix(varPartP)),SubSampling=paste(sort(Subsample),collapse="-"))
+	colnames(df)<-c("Gene","Variable","Variance","SubSampling")
+	df$Variance<-round(df$Variance,digits=4)
+	Total<-rbind(Total,df)
 }
+
 write.table(file=argsL$OutHost,Total,row.names=F,sep="\t",quote=F)
